2f908851905cae53f3f011e051f4acac3b916e3f
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-10 15:17:20 -0500

    feat: first commit, backend

---
+++ src/main/java/com/instalert_backend/InstalertBackendApplication.java
@@ -0,0 +1,13 @@
+package com.instalert_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstalertBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstalertBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instalert_backend/InstalertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instalert_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstalertBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
