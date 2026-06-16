c0338c63a1092236ed44a3090562068f5eb7f64c
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-16 16:17:11 -0500

    feat(profiles): add CreateUserCommand record for user creation

---
+++ src/main/java/com/instalert_backend/profiles/domain/model/commands/CreateUserCommand.java
@@ -0,0 +1,11 @@
+package com.instalert_backend.profiles.domain.model.commands;
+
+public record CreateUserCommand(
+        String name,
+        String email,
+        String password,
+        String role,
+        String currentLocation,
+        String avatar,
+        String phone
+) {}
