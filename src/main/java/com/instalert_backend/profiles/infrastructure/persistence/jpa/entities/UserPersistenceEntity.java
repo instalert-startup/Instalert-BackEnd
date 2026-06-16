793d779a8cda37cf0169e30b2b64c71e1a938b84
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-16 16:18:41 -0500

    feat(profiles): add UserPersistenceEntity for user data persistence

---
+++ src/main/java/com/instalert_backend/profiles/infrastructure/persistence/jpa/entities/UserPersistenceEntity.java
@@ -0,0 +1,29 @@
+package com.instalert_backend.profiles.infrastructure.persistence.jpa.entities;
+
+import com.instalert_backend.shared.infrastructure.persistence.jpa.entities.AuditableAbstractPersistenceEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserPersistenceEntity extends AuditableAbstractPersistenceEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+    private String currentLocation;
+    private String avatar;
+    private String phone;
+
+    public UserPersistenceEntity() {}
+}
