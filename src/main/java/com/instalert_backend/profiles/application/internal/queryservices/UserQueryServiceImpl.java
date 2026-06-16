6bfb7761d9b91952616f37d7fc9b9236017d339b
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-16 16:19:02 -0500

    feat(profiles): implement UserQueryService for querying user data

---
+++ src/main/java/com/instalert_backend/profiles/application/internal/queryservices/UserQueryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.instalert_backend.profiles.application.internal.queryservices;
+
+import com.instalert_backend.profiles.application.queryservices.UserQueryService;
+import com.instalert_backend.profiles.domain.model.aggregates.User;
+import com.instalert_backend.profiles.domain.model.queries.GetAllUsersQuery;
+import com.instalert_backend.profiles.domain.model.queries.GetUserByIdQuery;
+import com.instalert_backend.profiles.domain.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserQueryServiceImpl implements UserQueryService {
+
+    private final UserRepository userRepository;
+
+    public UserQueryServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> handle(GetAllUsersQuery query) {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> handle(GetUserByIdQuery query) {
+        return userRepository.findById(query.id());
+    }
+}
