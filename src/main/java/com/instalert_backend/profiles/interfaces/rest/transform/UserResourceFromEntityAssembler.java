c06c52ae56e19517b3929fcc8e5a4639e7bf3b97
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-16 16:19:32 -0500

    feat(profiles): add UserResourceFromEntityAssembler for user resource transformation

---
+++ src/main/java/com/instalert_backend/profiles/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -0,0 +1,19 @@
+package com.instalert_backend.profiles.interfaces.rest.transform;
+
+import com.instalert_backend.profiles.domain.model.aggregates.User;
+import com.instalert_backend.profiles.interfaces.rest.resources.UserResource;
+
+public class UserResourceFromEntityAssembler {
+
+    public static UserResource toResourceFromEntity(User user) {
+        return new UserResource(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole(),
+                user.getCurrentLocation(),
+                user.getAvatar(),
+                user.getPhone()
+        );
+    }
+}
