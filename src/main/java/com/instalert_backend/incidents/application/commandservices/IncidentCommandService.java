fc0d479a54a054bbdb56834456a2c5f937e18f54
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-30 03:11:02 -0500

    feat: add DeleteIncidentCommand and updateStatus method in Incident class

--- src/main/java/com/instalert_backend/incidents/application/commandservices/IncidentCommandService.java
+++ src/main/java/com/instalert_backend/incidents/application/commandservices/IncidentCommandService.java
@@ -2,10 +2,16 @@ package com.instalert_backend.incidents.application.commandservices;
 
 import com.instalert_backend.incidents.domain.model.aggregates.Incident;
 import com.instalert_backend.incidents.domain.model.commands.CreateIncidentCommand;
+import com.instalert_backend.incidents.domain.model.commands.DeleteIncidentCommand;
+import com.instalert_backend.incidents.domain.model.commands.UpdateIncidentStatusCommand;
 import com.instalert_backend.shared.application.result.ApplicationError;
 import com.instalert_backend.shared.application.result.Result;
 
 public interface IncidentCommandService {
 
     Result<Incident, ApplicationError> handle(CreateIncidentCommand command);
+
+    Result<Void, ApplicationError> handle(DeleteIncidentCommand command);
+
+    Result<Incident, ApplicationError> handle(UpdateIncidentStatusCommand command);
 }
