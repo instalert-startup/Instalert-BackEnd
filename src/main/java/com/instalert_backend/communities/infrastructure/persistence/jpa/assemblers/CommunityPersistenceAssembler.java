aa42ffc750a6af93833c3ed13ceea59097b722e6
breithner1911 <breithner1911@gmail.com>
2026-07-04 21:30:52 -0500

    feat(communities): include ownerId in CommunityPersistenceAssembler for data transformation.

--- src/main/java/com/instalert_backend/communities/infrastructure/persistence/jpa/assemblers/CommunityPersistenceAssembler.java
+++ src/main/java/com/instalert_backend/communities/infrastructure/persistence/jpa/assemblers/CommunityPersistenceAssembler.java
@@ -6,23 +6,25 @@ import com.instalert_backend.communities.infrastructure.persistence.jpa.entities
 public final class CommunityPersistenceAssembler {
     private CommunityPersistenceAssembler() {}
 
-    // De Base de Datos a Dominio
+
     public static Community toDomainFromPersistence(CommunityPersistenceEntity entity) {
         return new Community(
                 entity.getId(),
                 entity.getName(),
                 entity.getDescription(),
-                entity.isPrivate()
+                entity.isPrivate(),
+                entity.getOwnerId()
         );
     }
 
-    // De Dominio a Base de Datos
+
     public static CommunityPersistenceEntity toPersistenceFromDomain(Community community) {
         var entity = new CommunityPersistenceEntity();
         entity.setId(community.getId());
         entity.setName(community.getName());
         entity.setDescription(community.getDescription());
         entity.setPrivate(community.isPrivate());
+        entity.setOwnerId(community.getOwnerId());
         return entity;
     }
 }
