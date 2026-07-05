9c29ac56643fca76f72b279cf6ca0943507f4422
breithner1911 <breithner1911@gmail.com>
2026-07-04 21:32:09 -0500

    feat(communities): add ownerId field to CreateCommunityCommand for community creation.

--- src/main/java/com/instalert_backend/communities/domain/model/commands/CreateCommunityCommand.java
+++ src/main/java/com/instalert_backend/communities/domain/model/commands/CreateCommunityCommand.java
@@ -3,5 +3,6 @@ package com.instalert_backend.communities.domain.model.commands;
 public record CreateCommunityCommand(
         String name,
         String description,
-        boolean isPrivate
+        boolean isPrivate,
+        Long ownerId
 ) {}
